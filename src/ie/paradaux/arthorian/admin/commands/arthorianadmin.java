65a10ebfc2efce2f7c0c58702e58bc1526953e09
Rían <rian@paradaux.ie>
2017-08-14 17:14:55 +0100

    FIXES MAIN.CLASS LOCATION

--- src/ie/paradaux/arthorian/admin/commands/arthorianadmin.java
+++ src/ie/paradaux/arthorian/admin/commands/arthorianadmin.java
@@ -11,35 +11,38 @@ import ie.paradaux.arthorian.api.staffmanager;
 
 public class arthorianadmin extends JavaPlugin implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] arg3) {
-		if (cmd.getName().equalsIgnoreCase("arthorianadmin") || cmd.getName().equalsIgnoreCase("admin") || cmd.getName().equalsIgnoreCase("aadmin") || cmd.getName().equalsIgnoreCase("aa") || cmd.getName().equalsIgnoreCase("a")) {
-			if(arg3[0]==null) {
-			language.aa(sender);
-			language.arthorianadmin(sender);
-			language.aa(sender);
-			return true; }
-			else if (arg3[0]=="freeze") {
+		if (cmd.getName().equalsIgnoreCase("arthorianadmin") || cmd.getName().equalsIgnoreCase("admin")
+				|| cmd.getName().equalsIgnoreCase("aadmin") || cmd.getName().equalsIgnoreCase("aa")
+				|| cmd.getName().equalsIgnoreCase("a")) {
+			if (arg3[0] == null) {
+				language.aa(sender);
+				language.arthorianadmin(sender);
+				language.aa(sender);
+				return true;
+			} else if (arg3[0] == "freeze") {
 				language.unavailable(sender);
 				return true;
-			}
-			else if (arg3[0]=="makestaff") {
-				if(sender.hasPermission("arthorian.admin") && sender.hasPermission("arthorian.netman")) {
+			} else if (arg3[0] == "makestaff") {
+				if (sender.hasPermission("arthorian.admin") && sender.hasPermission("arthorian.netman")) {
 					if (arg3.length == 2) {
-						// Player p = (Player) getServer().getPlayer(arg3[0]); // Turns
+						// Player p = (Player) getServer().getPlayer(arg3[0]);
+						// // Turns
 						// String arg3[0] into a player
-						staffmanager.makeStaff(sender, arg3[1], arg3[2]); // Makes them staff
+						staffmanager.makeStaff(sender, arg3[1], arg3[2]); // Makes
+																			// them
+																			// staff
 						return true;
 					}
 				}
-			}
-			else if (arg3[0]=="demote") {
-				if(sender.hasPermission("arthorian.admin") && sender.hasPermission("arthorian.netman")) {
+			} else if (arg3[0] == "demote") {
+				if (sender.hasPermission("arthorian.admin") && sender.hasPermission("arthorian.netman")) {
 					if (arg3.length == 1 && getServer().getPlayer(arg3[0]) instanceof Player) {
 						staffmanager.demote(sender, arg3[0]);
 						return true;
 					}
 				}
-				}
 			}
+		}
 		return false;
 	}
 }
